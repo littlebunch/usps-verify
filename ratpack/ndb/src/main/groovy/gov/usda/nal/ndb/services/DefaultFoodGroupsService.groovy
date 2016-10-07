a8d568b4abccc7dd9390d3bb4d1ea08a686ef11b
Gary Moore <littlebunch@gmail.com>
2016-10-06 20:45:31 -0400

    added getFoodGroupsAsJson

--- ratpack/ndb/src/main/groovy/gov/usda/nal/ndb/services/DefaultFoodGroupsService.groovy
+++ ratpack/ndb/src/main/groovy/gov/usda/nal/ndb/services/DefaultFoodGroupsService.groovy
@@ -2,6 +2,7 @@ package gov.usda.nal.ndb.services
 import groovy.json.JsonBuilder
 import ratpack.exec.Promise
 import ratpack.exec.Operation
+import ratpack.exec.Blocking
 //import org.apache.commons.httpclient.HttpStatus
 import gov.usda.nal.ndb.model.FoodGroups
 class DefaultFoodGroupsService implements FoodGroupsService {
@@ -25,23 +26,23 @@ Promise<String> save(FoodGroups f)
   Promise<List<FoodGroups>> getFoodGroups() {
     def f
       FoodGroups.withNewSession {
+
         FoodGroups.list().collect(f) {
           [id:it.id,cd:it.cd,description:it.description]
         }
-      }
+    }
       Promise.sync{f}
   }
   Promise<String> getFoodGroupsAsJson()
   {
     JsonBuilder j=new JsonBuilder()
-    FoodGroups.withNewSession {
-      j {
-
-      foodgroup( FoodGroups.list().collect
-        {
-          [id:it.id,cd:it.cd,description:it.description,lastUpdated:it.lastUpdated]
-        })
-      }
+    j {
+      foodgroup(
+          FoodGroups.withNewSession {
+            FoodGroups.list().collect{
+              [id:it.id,cd:it.cd,description:it.description,lastUpdated:it.lastUpdated]
+          }}
+      )
     }
     Promise.sync{ j.toString()}
   }
