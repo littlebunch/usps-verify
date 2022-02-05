edca667ed92045d1049a49db09faed4f23931553
Gary Moore <littlebunch@gmail.com>
2022-02-05 09:46:20 -0500

    refactored for Typescript

---
+++ usps-react/src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import {Address} from "./Address";
+
+
+function App() {
+  
+  return (
+    <Address />
+  );
+  
+    
+}
+
+export default App;
---
+++ usps-react/src/Styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+export const Fieldset = styled.fieldset`
+  margin: 10px auto 0 auto;
+  padding: 30px;
+  width: 90%;
+  background-color: '#f7f8fa';
+  border-radius: 4px;
+  border: 1px solid '#e3e2e2';
+  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+`;
