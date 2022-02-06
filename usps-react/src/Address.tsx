24cfe82bda83022dcd309a6d2003ae3d7479d419
Gary Moore <littlebunch@gmail.com>
2022-02-06 17:11:25 -0500

    finished validateAddress draft

--- usps-react/src/Address.tsx
+++ usps-react/src/Address.tsx
@@ -3,16 +3,15 @@ import React, { useState, useEffect } from "react";
 import { ObjectType, ObjectTypeDeclaration } from "typescript";
 import { useForm } from 'react-hook-form';
 import "./App.css";
-import {Fieldset} from './Styles';
+import {FieldLabel,FieldError,Fieldset,FieldInput,ActionButton,SubmissionSuccess,SubmissionFailure} from './Styles';
 
 type FormData = {
+    address1: string,
+    address2: string,
+    zipcode: string,
     city: string,
     state: string,
-    zipcode: string,
-    address1: string,
-    address2: string 
   };
-
 const xml2json = (srcDOM: any) => {
   var jsonResult: any = {};
   
@@ -43,124 +42,173 @@ const xml2json = (srcDOM: any) => {
   return jsonResult;
 };
 export const Address = () => {
-  
+  const [address1,setAddress1] = useState("");
+  const [address2,setAddress2] = useState("");
   const initialCityState = {city: "", state: "" };
   const [cityState, setCityState] = useState(initialCityState);
   const [zipcode, setZipcode] = useState("");
   const [loading, setLoading] = useState(false);
   const isZipValid = zipcode.length === 5 && zipcode;
   const parser = new DOMParser();
-  const address1 = useState(false);
   const [successfullySubmitted, setSuccessfullySubmitted] =
   React.useState(false);
-  useEffect(() => {
-    const fetchCityState = async () => {
-      try {
-        if ( isZipValid ) {
-          const response = await fetch(
-            `/.netlify/functions/getCityState?zipcode=${zipcode}`,
-            {headers: { accept: "application/json"}}
-          );
-          const data = await response.text();
-          setLoading(false);
-          const srcDom=parser.parseFromString(data,"application/xml");
-          const res=xml2json(srcDom);
-          //console.log(xml2json(srcDom));
-          //console.log(srcDom);
-          if ( res?.CityStateLookupResponse?.ZipCode?.City) {
-            setLoading(false);
-            setCityState({...cityState, 
-              city: res.CityStateLookupResponse.ZipCode.City, 
-              state: res.CityStateLookupResponse.ZipCode.State,
-            });
-          } else if (res?.CityStateLookupResponse?.ZipCode?.Error) {
-            setLoading(false);
-            setCityState({
-              ...cityState,
-              city: `Invalid Zip Code for ${zipcode}`,
-              state: "Try Again",
-            });
-          }
-        }
-      } catch(e) {
-        console.log(e);
-      }
-    };
-    fetchCityState();
-  },[zipcode]);
-  const {handleSubmit, formState } = useForm<FormData>({
+  const [unsuccessfullySubmitted, setUnsuccessfullySubmitted] =
+  React.useState(false);
+  const {handleSubmit,formState:{errors}, setError, clearErrors, formState, register,reset } = useForm<FormData>({
     mode: 'onBlur',
 });
-const submitForm = (data: FormData) => {
-    // Preventing the page from reloading
-    console.log(address1);
-    console.log(zipcode);
-    console.log(cityState.city);
-    console.log(cityState.state);
-    setSuccessfullySubmitted(true);
-    
-  }
+  useEffect(() => {
+    if ( isZipValid ) {
+        fetchCityState();
+    }
+  },[zipcode]);
+async function fetchCityState ()  {
+    try {
+        const response = await fetch(
+          `/.netlify/functions/getCityState?zipcode=${zipcode}`,
+          {headers: { accept: "application/json"}}
+        );
+        const data = await response.text();
+        setLoading(false);
+        const srcDom=parser.parseFromString(data,"application/xml");
+        const res=xml2json(srcDom);
+        if ( res?.CityStateLookupResponse?.ZipCode?.City) {
+          setLoading(false);
+          clearErrors("zipcode");
+          setCityState({...cityState, 
+            city: res.CityStateLookupResponse.ZipCode.City, 
+            state: res.CityStateLookupResponse.ZipCode.State,
+          });
+        } else if (res?.CityStateLookupResponse?.ZipCode?.Error) {
+          setLoading(false);
+          setError("zipcode",{message:"Please enter a valid zipcode."})
+          setCityState({
+            ...cityState,
+            city: "",
+            state: "",
+          });
+        
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  };
+  
+ async function validateAddress() {
+    const response = await fetch(
+        `/.netlify/functions/validateForm?zipcode=${zipcode}&address1=${address1}&address2=${address2}`,
+        {headers: { accept: "application/json"}}
+    );
+    const data = await response.text();
+    setLoading(false);
+    const srcDom=parser.parseFromString(data,"application/xml");
+    const res=xml2json(srcDom);
+    if ( res.AddressValidateResponse.Address.Error) {
+        setUnsuccessfullySubmitted(true);
+         setSuccessfullySubmitted(false);
+    } else {
+        if ( !res.AddressValidateResponse.Address.Address1  ) {
+            setAddress1(res.AddressValidateResponse.Address.Address2);
+            setAddress2("");
+        } else {
+            console.log(res.AddressValidateResponse.Address.Address2);
+            setAddress1(res.AddressValidateResponse.Address.Address1);
+            setAddress2(res.AddressValidateResponse.Address.Address2);
+        }
+        setSuccessfullySubmitted(true);
+        setUnsuccessfullySubmitted(false);
+    }
+ }
+ 
   return (
-  <div className="App">
-    <h1>USPS Zip API</h1>
-    <Fieldset disabled={formState.isSubmitting || successfullySubmitted}>
-    <form onSubmit={handleSubmit(submitForm)} className="form-data">
-      <label htmlFor="address1">Street Address 1</label>
-      <input className="address"
+  <div>
+    <h1>USPS Address API</h1>
+    <Fieldset disabled={formState.isSubmitting }>
+    <form onSubmit={handleSubmit(validateAddress)} >
+   <FieldLabel htmlFor="address1">Address 1</FieldLabel>
+      <FieldInput
+      value = {address1}
       type="text"
       name="address1"
-      id="address1"/>
+      id="address1"
+      width={"25%"}
+      onChange={(event) => {
+        const { value } = event.target;
+        setAddress1(value);
+      }}/>
       <br/>
-      <label htmlFor="address2">Street Address 2</label>
-      <input className="address"
+      <FieldLabel htmlFor="address2">Address 2</FieldLabel>
+      <FieldInput
+      value={address2}
       type="text"
       name="address2"
-      id="address2"/>
+      id="address2"
+      width={"25%"}
+      onChange={(event) => {
+        const { value } = event.target;
+        setAddress2(value);
+      }}/>
       <br/>
-      <label htmlFor="zip">Zip Code</label>
-      <input
-      className="zip"
+      <FieldLabel htmlFor="zipcode">Zip Code</FieldLabel>
+      <FieldInput
       value={zipcode || ""}
       placeholder="XXXXX"
       type="text"
-      name="zip"
-      id="zip"
+      id="zipcode"
+      width={"5%"}
+      {...register("zipcode",{required: "Please enter a valid zipcode.",
+      })}
       onChange={(event) => {
         const { value } = event.target;
         setLoading(true);
         setZipcode(value.replace(/[^\d{5}]$/,"").substring(0,5));
       }}
       />
-      <label htmlFor="city">City</label>
-      <input
-      className={`city`}
+      
+      <FieldLabel htmlFor="city">City</FieldLabel>
+      <FieldInput
       value={cityState.city}
       type="text"
       name="city"
       disabled
       id="city"
+      width={"20%"}
       />
       <div className="icon-container">
             <i className={`${loading && isZipValid ? "loader" : ""}`}></i>
           </div>
-      <label htmlFor="state">State</label>
-      <input
-      className={`state`}
+      <FieldLabel htmlFor="state">State</FieldLabel>
+      <FieldInput
       value={cityState.state}
       type="text"
       name="state"
       disabled
       id="state"
+      width={"5%"}
       />
+      {errors.zipcode && <FieldError>{errors.zipcode.message}</FieldError>}
       <br/>
-      <button type="submit">Save</button>
-      <div className="icon-container">
-        <i className={`loading && isZipValue ? "loader" : ""`}>
-        </i>
-      </div>
-    </form>
+      
+    
+      <ActionButton type="submit" onClick={()=> clearErrors()}
+      color="#4CAF50">Validate</ActionButton>
+      
+          <ActionButton type='reset'
+          onClick={() => reset()}
+            color="red" >Reset</ActionButton>
+        {successfullySubmitted && (
+            <SubmissionSuccess>
+              ADDRESS VALIDATED
+            </SubmissionSuccess>
+          )}
+          {unsuccessfullySubmitted && (
+              <SubmissionFailure>
+                  ADDRESS NOT VALIDATED
+              </SubmissionFailure>
+          )}
+   </form>
+   </Fieldset>
 
-    </Fieldset>
     
   </div>
   );
--- usps-react/src/Styles.ts
+++ usps-react/src/Styles.ts
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+type InputProp = {
+  width: string
+}
+type ButtonProp = {
+  color:string
+}
 export const Fieldset = styled.fieldset`
   margin: 10px auto 0 auto;
   padding: 30px;
@@ -8,4 +14,52 @@ export const Fieldset = styled.fieldset`
   border-radius: 4px;
   border: 1px solid '#e3e2e2';
   box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+`;
+export const ActionButton = styled.button<ButtonProp>`
+  background-color: ${props =>
+      props.color }; 
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin:5px;
+  :hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    font-weight: bold;
+  }
+  focus {
+    outline-color: #824c67;
+  }
+  :disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+  `;
+  export const FieldInput = styled.input<InputProp>`
+    font-size: 1.2rem;
+    margin: 10px;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    width: ${props =>
+      props.width };
+  `;
+  export const FieldLabel = styled.label`
+  font-weight: bold;
+  font-size: 1.2rem;`;
+
+  export const SubmissionSuccess = styled.div`
+  margin-top: 10px;
+  color: green;
+`;
+export const SubmissionFailure = styled.div`
+  margin-top: 10px;
+  color: red;
+`;
+export const FieldError = styled.div`
+  font-size: 12px;
+  color: red;
 `;
